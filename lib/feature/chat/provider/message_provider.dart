391dbacde1c9838d11e628dff0c3a922f9c4bebe
piyushmahajan7749 <piyushmahajan7749@gmail.com>
2024-11-05 07:08:24 +0530

    phone working

--- lib/feature/chat/provider/message_provider.dart
+++ lib/feature/chat/provider/message_provider.dart
@@ -269,7 +269,7 @@ class MessageListNotifier extends StateNotifier<ChatBot> {
           );
           messageText.writeln('');
           messageText.writeln(
-            'Contact: ${property['name']} ${property['contact_number']}',
+            'Added by: ${property['name']} ${property['contact_number'].toString().replaceFirst("+91", '').replaceAll(' ', '')}',
           );
 
           final chatMessage = ChatMessage(
